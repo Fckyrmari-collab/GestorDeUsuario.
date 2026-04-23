78c6b856b47c63f8ce9a63520ab283fead597e40
Fckyrmari-collab <dinosolispau679@gmail.com>
2026-04-22 19:40:07 -0500

    Refactor conectar function in database.py
    Refactor database connection function to remove unnecessary calls.

--- database.py
+++ database.py
@@ -1,11 +1,7 @@
 import mysql.connector
 
 def conectar():
-<<<<<<< HEAD
-    
-=======
         #conectar a la base de datos
->>>>>>> 97b7d75405575abc9eacc314ba50e0c2561240b4
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -16,7 +12,3 @@ def conectar():
         print("conexion a la base de datos realizada correctamente")
     
     return conn
-<<<<<<< HEAD
-=======
-conectar()
->>>>>>> 97b7d75405575abc9eacc314ba50e0c2561240b4
